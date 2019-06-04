5c7952f7bc64b45c4eba5fb3000a48a6d35aaa9b
konyka <konyka@126.com>
2019-06-04 22:57:39 +0800

    add public memory.h

---
+++ src/include/memory.h
@@ -0,0 +1,104 @@
+/**
+ *===========================================================================
+ *  None Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: memory.h
+ *      Version: v0.0.0
+ *   Created on: 2015-05-05 20:49:43 by konyka
+ *  Modified by: konyka
+ *Modified time: 2019-06-04 22:48:30
+ *       Editor: Sublime Text3
+ *        Email: 
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History: 
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_MEMORY_H__
+#define __DARKBLUE_MEMORY_H__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+    MEM_FAIL_AND_EXIT,
+    MEM_FAIL_AND_RETURN,
+} mem_fail_mode;
+
+typedef struct mem_controller_tag *mem_controller;
+typedef void (*mem_errorHandler)(mem_controller, char *, int, char *);
+typedef struct mem_storage_tag *mem_storage;
+
+extern mem_controller mem_default_controller;
+
+#ifdef MEM_CONTROLLER
+#define MEM_CURRENT_CONTROLLER MEM_CONTROLLER
+#else /* MEM_CONTROLLER */
+#define MEM_CURRENT_CONTROLLER mem_default_controller
+#endif /* MEM_CONTROLLER */
+
+/*
+ * Don't use mem_*_func function.
+ * They are private functions of memory module.
+ */
+mem_controller mem_create_controller(void);
+void *mem_malloc_func(mem_controller controller,
+                      char *filename, int line, size_t size);
+void *mem_realloc_func(mem_controller controller,
+                       char *filename, int line, void *ptr, size_t size);
+char *mem_strdup_func(mem_controller controller,
+                      char *filename, int line, char *str);
+mem_storage mem_open_storage_func(mem_controller controller,
+                                  char *filename, int line, int page_size);
+void *mem_storage_malloc_func(mem_controller controller,
+                              char *filename, int line,
+                              mem_storage storage, size_t size);
+void mem_free_func(mem_controller controller, void *ptr);
+void mem_dispose_storage_func(mem_controller controller,
+                              mem_storage storage);
+
+void mem_set_error_handler(mem_controller controller,
+                           mem_errorHandler handler);
+void mem_set_fail_mode(mem_controller controller,
+                       mem_fail_mode mode);
+void mem_dump_blocks_func(mem_controller controller, FILE *fp);
+void mem_check_block_func(mem_controller controller,
+                          char *filename, int line, void *p);
+void mem_check_all_blocks_func(mem_controller controller,
+                               char *filename, int line);
+
+#define mem_malloc(size)\
+  (mem_malloc_func(mem_current_controller,\
+                   __FILE__, __LINE__, size))
+#define mem_realloc(ptr, size)\
+  (mem_realloc_func(mem_current_controller, __FILE__, __LINE__, ptr, size))
+#define mem_strdup(str)\
+  (mem_strdup_func(mem_current_controller, __FILE__, __LINE__, str))
+#define mem_open_storage(page_size)\
+  (mem_open_storage_func(mem_current_controller,\
+                         __FILE__, __LINE__, page_size))
+#define mem_storage_malloc(storage, size)\
+  (mem_storage_malloc_func(mem_current_controller,\
+                           __FILE__, __LINE__, storage, size))
+#define mem_free(ptr) (mem_free_func(mem_current_controller, ptr))
+#define mem_dispose_storage(storage)\
+  (mem_dispose_storage_func(mem_current_controller, storage))
+#ifdef DEBUG
+#define mem_dump_blocks(fp)\
+  (mem_dump_blocks_func(mem_current_controller, fp))
+#define mem_check_block(p)\
+  (mem_check_block_func(mem_current_controller, __FILE__, __LINE__, p))
+#define mem_check_all_blocks()\
+  (mem_check_all_blocks_func(mem_current_controller, __FILE__, __LINE__))
+#else /* DEBUG */
+#define mem_dump_blocks(fp) ((void)0)
+#define mem_check_block(p)  ((void)0)
+#define mem_check_all_blocks() ((void)0)
+#endif /* DEBUG */
+
+#endif /* __DARKBLUE_MEMORY_H__ */
